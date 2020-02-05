9a9e41a5705a056c0fed3cf18a9276228cb593ff
Diego Vasquez <botellongo69@outlook.com>
2020-02-05 21:20:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,6 +17,10 @@ class Artist
     end
   end
   
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
   def genres
     songs.map do |song|
       song.genre
--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
+    @@all << self
   end
   
   def self.all
