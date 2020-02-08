4f210a8f14f0f705b8214d973771908f67a5e94e
Kyle McKenna <mckennakyler@gmail.com>
2020-02-08 04:33:23 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist 
+  
+  @@all = [] 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    
+    
+    save 
+  end 
+  
+  def self.all 
+    @@all
+  end   
+  
+  def save 
+    @@all << self 
+  end   
+  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end   
+  
+  def genres 
+    songs.collect {|song| song.genre}
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
+  
+end 
---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    
+    save 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def save 
+    @@all << self 
+  end   
+ 
+  def songs 
+    Song.all {|song| song.genre == self}
+  end   
+
+  def artists
+    songs.collect {|song| song.artist}
+  end 
+  
+end  
